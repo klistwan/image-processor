b4f252898e41502d5a5ae538cc92dc5fbe2f979c
Konrad Listwan-Ciesielski <konrad.listwan@gmail.com>
2020-05-23 09:28:53 +0900

    Download image from given url

--- api.py
+++ api.py
@@ -1,29 +1,40 @@
+import json
 import os
+import requests
+import shutil
 
 from flask import Flask, abort, request, jsonify, Response
 from PIL import Image
-from json import dumps
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
+def download_image(url):
+	response = requests.get(url, stream=True)
+	with open('temporary.jpeg', 'wb') as out_file:
+		shutil.copyfileobj(response.raw, out_file)
+	del response
+	return 'temporary.jpeg'
 
 def resize(filepath):
 	image = Image.open(filepath)
 	image.thumbnail((100,100))
 	new_url = "thumbnail.jpg"
 	image.save(new_url)
+	os.remove(filepath)
 	return new_url
 
-
-@app.route('/resize', methods=['POST'])
+@app.route('/images', methods=['POST'])
 def home():
 	if not request.json or not 'url' in request.json:
 		abort(400)
-	if not os.path.exists(request.json['url']):
-		error_message = dumps({'Message': 'Photo not found.'})
+	response = requests.get(request.json['url'], stream=True)
+	if response.status_code == 404:
+		error_message = json.dumps({'Message': 'Photo not found.'})
 		abort(Response(error_message, 404))
-	resized_url = resize(request.json['url'])
+	# Download image.
+	local_url = download_image(request.json['url'])
+	resized_url = resize(local_url)
 	return jsonify({'url': resized_url}, 201)
 
 
